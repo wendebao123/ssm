307d59c87d36afb4a1bbe7d5375c39be53c8c0ac
wendebao <wwsscca@163.com>
2019-03-22 13:53:17 +0800

    添加工程到暂存区

---
+++ get_test/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+    }
+}
